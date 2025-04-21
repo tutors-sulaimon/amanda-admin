e0b75ee2c70c0cbfd86238d23c16418115424c3f
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-04-21 15:02:15 +0300

    Latest database setup

--- config/database.ts
+++ config/database.ts
@@ -1,37 +1,37 @@
-    // strapi-api/config/database.js
-    export default ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed SSL certificates
-          },
-        },
-        debug: false,
+    // // strapi-api/config/database.js
+    // export default ({ env }) => ({
+    //   connection: {
+    //     client: 'postgres',
+    //     connection: {
+    //       host: env('DATABASE_HOST'),
+    //       port: env.int('DATABASE_PORT'),
+    //       database: env('DATABASE_NAME'),
+    //       user: env('DATABASE_USERNAME'),
+    //       password: env('DATABASE_PASSWORD'),
+    //       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+    //       ssl: {
+    //         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed SSL certificates
+    //       },
+    //     },
+    //     debug: false,
+    //   },
+    // });
+
+
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST'),
+      port: env.int('PGPORT'),
+      database: env('PGDATABASE'),
+      user: env('PGUSER'),
+      password: env('PGPASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
-    });
-
-
-// export default ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('PGHOST'),
-//       port: env.int('PGPORT'),
-//       database: env('PGDATABASE'),
-//       user: env('PGUSER'),
-//       password: env('PGPASSWORD'),
-//       schema: env('DATABASE_SCHEMA', 'public'),
-//       ssl: {
-//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-//       },
-//     },
-//     debug: false,
-//   },
-// });
+    },
+    debug: false,
+  },
+});
